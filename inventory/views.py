d60c5e8f229f6ed5e0870c2e57dd7c55175d05d5
mkornyev <mkornyev@gmail.com>
2021-03-13 12:20:22 -0500

    Added skeleton project templates and directories
    -added static directory - for JS, CSS, and static assets
    -added template dir - for all our templates & partials
    -added home & about routes & added them to the home page

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
-# Create your views here.
+# IMPORTS 
+
+from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseRedirect #, JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+
+
+# BASIC VIEWS 
+
+def home(request): 
+	return render(request, 'inventory/home.html')
+
+def about(request):
+	return render(request, 'inventory/about.html')
