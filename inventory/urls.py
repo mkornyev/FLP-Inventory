6a835e2f40ee69be4f82bc91a72a3a1b753095c5
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-03-15 15:52:35 -0400

    Fixed linter issues

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+# from django.urls import path
+# from django.contrib.auth import views as auth_views
+# from . import views
 
-urlpatterns = [
-]
+# urlpatterns = [
+# ]
 
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
