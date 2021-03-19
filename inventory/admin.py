b692168c8ca1511432496cdd09ce9fa5b463ad4f
Lydia Xing <xinglydia@gmail.com>
2021-03-19 16:05:55 -0400

    Add script to populate models (#18)
    * add models to admin site
    
    * add populate script
    
    * Update README.md
    
    * switch to 4 spaces

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import Family, Category, Item, ItemTransaction, Checkin, Checkout
+
+admin.site.register(Family)
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(ItemTransaction)
+admin.site.register(Checkin)
+admin.site.register(Checkout)
