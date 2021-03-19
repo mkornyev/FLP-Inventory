bb714a9dde7e4a61ef0b0bbcaf9bd6516896c30d
Lydia Xing <xinglydia@gmail.com>
2021-03-19 16:05:55 -0400

    Add script to populate models (#18)
    * add models to admin site
    
    * add populate script
    
    * Update README.md
    
    * switch to 4 spaces

--- inventory/management/commands/populate.py
+++ inventory/management/commands/populate.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-# from inventory.models import User
+from inventory.models import User, Family, Category, Item, ItemTransaction, Checkin, Checkout
 
 # POPULATE SCRIPT
 
@@ -7,22 +7,48 @@ class Command(BaseCommand):
     args = '<this func takes no args>'
     help = 'Run this script to create sample model objects.'
 
-    # some sample code i stole from another project:
-    
-    # def _create_users(self):
-    #     """
-    #     sow = User.objects.create_user(username='sow', password='sow', first_name='Max', last_name='K', email='mkornyev@gmail.com')
-    #     sow.is_staff = True 
-    #     sow.is_superuser = False
-    #     sow.save()
-    #     """
-    #     admin = User.objects.create_user(username='admintaili', password='gy!e12uNAs', first_name='Taili', last_name='Thompson', email='bvbaseball42@gmail.com')
-    #     admin.is_staff = True 
-    #     admin.is_superuser = True
-    #     admin.save()
+    def _create_users(self):
+        admin = User.objects.create_user(username='admintaili', password='gy!e12uNAs', first_name='Taili', last_name='Thompson', email='bvbaseball42@gmail.com')
+        admin.is_staff = True 
+        admin.is_superuser = True
+        admin.save()
         
-    #     print("\nAdmin has been created.\n")
+        print("\nAdmin has been created.\n")
 
+    def _create_sample_objects(self):
+        sow = User.objects.create_user(username='sow', password='sow', first_name='Max', last_name='K', email='mkornyev@gmail.com')
+        sow.is_staff = True 
+        sow.is_superuser = False
+        sow.save()
+
+        print("Staff has been created.\n")
+
+        family = Family.objects.create(name="Jones-Indiana")
+        family.save()
+        print("Family has been created.\n")
+
+        category = Category.objects.create(name="Clothes")
+        category.save()
+        print("Category has been created.\n")
+
+        shirt = Item.objects.create(category=category, name="tshirt boys 4", price=10.56, quantity=10)
+        shirt.save()
+        print("Item has been created.\n")
+
+        transaction = ItemTransaction.objects.create(item=shirt, quantity=2)
+        transaction.save()
+        print("Transaction has been created.\n")
+
+        checkin = Checkin.objects.create(user=sow)
+        checkin.items.add(transaction)
+        checkin.save()
+        print("Checkin has been created.\n")
+
+        checkout = Checkout.objects.create(user=sow, family=family)
+        checkout.items.add(transaction)
+        checkout.save()
+        print("Checkout has been created.\n")
+    
     def handle(self, *args, **options):
-      pass
-        # self._create_users()
+        self._create_users()
+        self._create_sample_objects()
