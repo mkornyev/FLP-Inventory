b5cadeb35839c05440a0d2f923211e44e48e4fb7
mkornyev <mkornyev@gmail.com>
2021-03-19 16:00:16 -0400

    Updated readme and added model __str__ functions

--- inventory/models.py
+++ inventory/models.py
@@ -8,29 +8,38 @@ from datetime import datetime
 # MODELS 
 
 class Family(models.Model):
-  name = models.CharField(max_length=50, blank=False, null=False)
-  # first_seen = models.DateTimeField(default=datetime.now)
+	name = models.CharField(max_length=50, blank=False, null=False)
+	# first_seen = models.DateTimeField(default=datetime.now)
+
+	def __str__(self):
+		return self.name
 
 class Category(models.Model):
 	name = models.CharField(max_length=50, blank=False, null=False)
-	
+
+	def __str__(self):
+		return self.name 
+
 class Item(models.Model):
-  category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True) # CASCADE - deletes all items if a Category is deleted
-  name = models.CharField(max_length=50, blank=False, null=False)
-  price = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True) 
-  quantity = models.IntegerField(default=0)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True) # CASCADE - deletes all items if a Category is deleted
+	name = models.CharField(max_length=50, blank=False, null=False)
+	price = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True) 
+	quantity = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.name
 
 class ItemTransaction(models.Model):
-  item = models.ForeignKey(Item, on_delete=models.PROTECT, blank=True, null=True)
-  quantity = models.IntegerField(default=0)
-  
+	item = models.ForeignKey(Item, on_delete=models.PROTECT, blank=True, null=True)
+	quantity = models.IntegerField(default=0)
+
 class Checkin(models.Model):
-  user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
-  datetime = models.DateTimeField(default=datetime.now)
-  items = models.ManyToManyField(ItemTransaction, blank=False)
+	user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
+	datetime = models.DateTimeField(default=datetime.now)
+	items = models.ManyToManyField(ItemTransaction, blank=False)
 
 class Checkout(models.Model):
-  user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
-  family = models.ForeignKey(Family, on_delete=models.PROTECT, blank=True, null=True)
-  items = models.ManyToManyField(ItemTransaction, blank=False)
-  datetime = models.DateTimeField(default=datetime.now)
+	user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
+	family = models.ForeignKey(Family, on_delete=models.PROTECT, blank=True, null=True)
+	items = models.ManyToManyField(ItemTransaction, blank=False)
+	datetime = models.DateTimeField(default=datetime.now)
