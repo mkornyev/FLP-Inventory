f2b9b32757ebf5b695843ad45177296739b7e768
Lydia Xing <xinglydia@gmail.com>
2021-03-20 19:48:25 -0400

    Add script to drop all objects from DB (#20)
    * add models to admin site
    
    * add populate script
    
    * Update README.md
    
    * switch to 4 spaces
    
    * add drop command
    
    * update readme
    
    * add newline
    
    * shorten method of deleting objects
    
    * fix lint

--- inventory/management/commands/drop.py
+++ inventory/management/commands/drop.py
@@ -1,22 +1,52 @@
 from django.core.management.base import BaseCommand
-# from inventory.models import User
+from inventory.models import User, Family, Category, Item, ItemTransaction, Checkin, Checkout
 
 # DROP SCRIPT
 
 class Command(BaseCommand):
     args = '<this func takes no args>'
-    help = 'Run this script to create sample users.'
+    help = 'Run this script to destroy all objects.'
 
-    # some sample code I stole from an old project: 
-    
-    # def _destroy_users(self):
-    #     users = User.objects.all()
+    def _destroy_users(self):
+        User.objects.all().delete() 
 
-    #     for u in users:
-    #         u.delete() 
+        print("\nUsers deleted.\n")
 
-    #     print("\nUsers deleted.\n")
+    def _destroy_families(self):
+        Family.objects.all().delete() 
+
+        print("\nFamilies deleted.\n")
+
+    def _destroy_categories(self):
+        Category.objects.all().delete() 
+
+        print("\nCategories deleted.\n")
+
+    def _destroy_items(self):
+        Item.objects.all().delete() 
+
+        print("\nItems deleted.\n")
+
+    def _destroy_item_transactions(self):
+        ItemTransaction.objects.all().delete() 
+
+        print("\nItemTransactions deleted.\n")
+
+    def _destroy_checkins(self):
+        Checkin.objects.all().delete() 
+
+        print("\nCheckins deleted.\n")
+
+    def _destroy_checkouts(self):
+        Checkout.objects.all().delete() 
+
+        print("\nCheckouts deleted.\n")
 
     def handle(self, *args, **options):
-      pass
-      # self._destroy_users()
+        self._destroy_checkins()
+        self._destroy_checkouts()
+        self._destroy_item_transactions()
+        self._destroy_items()
+        self._destroy_categories()
+        self._destroy_families()
+        self._destroy_users()
