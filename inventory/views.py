7659a7f3258a3e9eaf31d0741c29ad9e46a86d79
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-03-22 14:34:42 -0400

    Added checkin add individual items and view

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core import serializers
 # from django.http import HttpResponse
 
 # from django.contrib.auth.decorators import login_required
@@ -9,7 +10,7 @@ from django.contrib.auth import authenticate, login, logout
 
 from inventory.forms import LoginForm
 from inventory.forms import RegistrationForm
-from inventory.forms import CheckinForm
+from inventory.forms import AddItemForm
 
 # BASIC VIEWS
 def home(request): 
@@ -69,41 +70,74 @@ def register_action(request):
     return redirect(reverse('Home'))
 
 # CHECKIN VIEWS
+def additem_action(request):
+    context = {}
+
+    if request.method == 'GET':
+        return redirect(reverse('Checkin'))
+
+    if request.method == 'POST':
+        form = AddItemForm(request.POST)
+        context['form'] = form
+
+        if not form.is_valid():
+            return render(request, 'inventory/checkin.html', context)
+
+        category = form.cleaned_data['category']
+        name = form.cleaned_data['name']
+        price = form.cleaned_data['price']
+        quantity = form.cleaned_data['quantity']
+
+        item = Item.objects.filter(name=name).first()
+        if not item:
+            newItem = Item(category=category, name=name, price=price)
+            newItem.save()
+            item = newItem
+    
+        tx = serializers.serialize("json", [ ItemTransaction(item=item, quantity=quantity), ])
+        if not 'transactions' in request.session or not request.session['transactions']:
+            request.session['transactions'] = [tx]
+        else:
+            saved_list = request.session['transactions']
+            saved_list.append(tx)
+            request.session['transactions'] = saved_list
+
+        return redirect(reverse('Checkin'))
+
+
 def checkin_action(request):
     context = {}
+        
+    # Create transactions if they don't exist
+    if not 'transactions' in request.session or not request.session['transactions']:
+        request.session['transactions'] = []
+
+    # Deserialize transactions 
+    serialized_transactions = request.session['transactions']
+    transactions = []
+    for tx in serialized_transactions:
+        for deserialized_transaction in serializers.deserialize("json", tx):
+            transactions.append(deserialized_transaction.object)
 
     if request.method == 'GET':
         context['items'] = Item.objects.all()
         context['categories'] = Category.objects.all()
-        context['form'] = CheckinForm()
+        context['form'] = AddItemForm()
+        context['transactions'] = transactions
         return render(request, 'inventory/checkin.html', context)
-
-    form = CheckinForm(request.POST)
-    context['form'] = form
-
-    if not form.is_valid():
-        return render(request, 'inventory/checkin.html', context)
-
-    category = form.cleaned_data['category']
-    name = form.cleaned_data['name']
-    price = form.cleaned_data['price']
-    quantity = form.cleaned_data['quantity']
-
-    item = Item.objects.filter(name=name).first()
-    if not item:
-        newItem = Item(category=category, name=name, price=price)
-        newItem.save()
-        item = newItem
-    
-    tx = ItemTransaction(item=item, quantity=quantity)
-    tx.save()
 
     checkin = Checkin()
     checkin.save()
-    # checkin = Checkin(user=request.user)
-    checkin.items.add(tx)
 
-    item.quantity += tx.quantity
-    item.save()
+    for tx in transactions:
+        tx.save()
+
+        checkin.items.add(tx)
+
+        tx.item.quantity += tx.quantity
+        tx.item.save()
+
+    del request.session['transactions']
+    request.session.modified = True
 
     return redirect(reverse('Home'))
