66561cf35124f9a3dad6c838538d15199a7b09f2
Alex Bellomo <40484273+alex-bellomo@users.noreply.github.com>
2021-03-29 17:40:31 -1000

    Checkin / Checkout Story (#42)
    Co-authored-by: Alex Bellomo <40484273+alex-bellomo@users.noreply.github.com>
    
    * Added bootstrap styling for check in and check out forms
    * Put items in cart into tables
    * Added back family option and styled with react
    * Added warning for no items added
    * Added user to checkin/checkout objects created through webapp
    
    Co-authored-by: Lydia Xing <xinglydia@gmail.com>
    Co-authored-by: Max Kornyev <mkornyev@gmail.com>

--- flp/urls.py
+++ flp/urls.py
@@ -32,11 +32,10 @@ urlpatterns = [
     # ACTION ROUTES
     path('report/', views.generate_report, name='Report'),
     path('checkin/', views.checkin_action, name='Checkin'),
-    path('additem/', views.additem_action, name='AddItem'),
-    path('admin/', admin.site.urls),
-
     path('checkout/', views.checkout_action, name='Checkout'),
-    path('additemout/', views.additemout_action, name='AddItemout'),
+    path('additem/<str:location>/', views.addtocart_action, name='AddItem'),
+    path('removeitem/<int:index>/<str:location>/', views.removeitem_action, name='RemoveItem'),
+
     path('families/index/', views.FamilyIndexView.as_view(), name="Families"),
     path('categories/index/', views.CategoryIndexView.as_view(), name="Categories"),
     path('items/index/', views.ItemIndexView.as_view(), name="Items"),
