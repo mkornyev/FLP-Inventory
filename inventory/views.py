7671a6aaeac0ddae1d9d065cfbbeb10e8496a899
Austin Leung <austinle@andrew.cmu.edu>
2021-04-03 15:08:26 -0400

    Fix bad merge

--- inventory/views.py
+++ inventory/views.py
@@ -208,51 +208,6 @@ def analytics(request):
 
     one_week_ago = date.today()-timedelta(days=7)
     context['one_week_ago'] = one_week_ago
-    all_checkouts = Checkout.objects.filter(datetime__date__gte=one_week_ago).all()
-
-    # Get checkouts grouped by items, sorted by quantity checked out
-    item_checkout_quantities = defaultdict(int)
-    for checkout in all_checkouts:
-        for itemTransaction in checkout.items.all():
-            item_obj = itemTransaction.item
-            quantity = itemTransaction.quantity
-            item_checkout_quantities[item_obj] += quantity
-
-    item_quant_tuples = item_checkout_quantities.items()
-
-    ### Sorting columns when pressed
-    default_order = 'checkout_quantity'
-    order_field = request.GET.get('order_by', default_order)
-
-    # switch sorting order each time
-    # the default sorting is "desc" ("asc" initially b/c "not" always happens)
-    non_default_sorting = "asc"
-    sort_type = request.GET.get('sort_type', non_default_sorting)
-    new_sort_type = "asc" if sort_type == "desc" else "desc" # switches sort_type
-    sort_reverse = new_sort_type == "desc"
-    context['sort_type'] = new_sort_type
-
-    order_lambda = lambda i_quantity: i_quantity[1] # default_order is checkout quantity
-    if order_field == 'item_quantity':
-        order_lambda = lambda i_quantity: i_quantity[0].quantity
-    elif order_field == 'name':
-        order_lambda = lambda i_quantity: i_quantity[0].name.lower()
-
-
-    context['most_checked_out'] = sorted(item_quant_tuples, key=order_lambda, reverse=sort_reverse)
-    context['most_checked_out'] = getPagination(request, context['most_checked_out'], DEFAULT_PAGINATION_SIZE)
-
-    context['LOW_QUANTITY_THRESHOLD'] = LOW_QUANTITY_THRESHOLD
-
-    return render(request, 'inventory/analytics.html', context)
-
-######################### ANALYTICS #########################
-@login_required
-def analytics(request):
-    context = {}
-
-    one_week_ago = date.today()-timedelta(days=7)
-    context['one_week_ago'] = one_week_ago
     all_checkouts = Checkout.objects
     past_week_checkouts = all_checkouts.filter(datetime__date__gte=one_week_ago).all()
 
