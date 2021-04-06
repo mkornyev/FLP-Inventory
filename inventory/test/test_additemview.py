04883905e837e113122d1519b8d6e7647354e32f
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-04-05 21:45:22 -0400

    Testing checkpoint

---
+++ inventory/test/test_additemview.py
@@ -0,0 +1,83 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
+
+from inventory.forms import AddItemForm
+from inventory.models import Item
+
+class AddItemTestCase(TestCase):
+    def setUp(self):
+        item = Item.objects.create(name="ValidItem", quantity=5)
+        item.save()
+
+        user = User.objects.create_superuser(username='testuser', password='12345')
+        user.save()
+
+    def test_invalid_not_logged_in(self):
+        response = self.client.get('/additem/in/')
+
+        # Check for redirect
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get('/additem/out/')
+
+        # Check for redirect
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post('/additem/in/')
+
+        # Check for redirect
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post('/additem/out/')
+
+        # Check for redirect
+        self.assertEqual(response.status_code, 302)
+
+    def test_get(self):
+        logged_in = self.client.login(username='testuser', password='12345')
+
+        response = self.client.get("/additem/in/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<h1>Add Item:</h1>")
+
+        response = self.client.get("/additem/out/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<h1>Add Item:</h1>")
+
+    def test_post_success(self):
+        logged_in = self.client.login(username='testuser', password='12345')
+
+        response = self.client.post(
+            "/additem/in/", data={"name": "ValidItem",
+                                  "quantity": 2}, follow=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(
+            "/additem/out/", data={"name": "ValidItem",
+                                   "quantity": 1}, follow=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        session = self.client.session
+
+        self.assertEqual(session['transactions-in'],
+                         ['[{"model": "inventory.itemtransaction", "pk": null, "fields": {"item": 1, "quantity": 2}}]'])
+        self.assertEqual(session['transactions-out'],
+                         ['[{"model": "inventory.itemtransaction", "pk": null, "fields": {"item": 1, "quantity": 1}}]'])
+
+    def test_post_error(self):
+        logged_in = self.client.login(username='testuser', password='12345')
+
+        response = self.client.post(
+            "/additem/in/", data={"name": "InvalidItem",
+                                  "quantity": 1}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Item does not exist.")
---
+++ inventory/test/test_checkoutform.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
+
+from inventory.forms import CheckOutForm
+from inventory.models import Family
+
+class CheckoutFormTestCase(TestCase):
+    def setUp(self):
+        family = Family.objects.create(name="ValidFamily")
+        family.save()
+
+    def test_invalid_required_fields(self):
+        form = CheckOutForm(data={})
+
+        self.assertEqual(
+            form.errors["family"], ["This field is required."]
+        )
+
+    def test_invalid_family(self):
+        form = CheckOutForm(data={"family": "This is a very long name that is over fifty characters"})
+
+        self.assertEqual(
+            form.errors["family"], ["Ensure this value has at most 50 characters (it has 54)."]
+        )
