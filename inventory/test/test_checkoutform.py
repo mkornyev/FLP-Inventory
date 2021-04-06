98ce9f06bc8857c170b13d706d87c7d6840d934d
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-04-05 21:59:04 -0400

    Finished tests and avoided datetime warning

--- inventory/test/test_checkoutform.py
+++ inventory/test/test_checkoutform.py
@@ -17,9 +17,16 @@ class CheckoutFormTestCase(TestCase):
             form.errors["family"], ["This field is required."]
         )
 
-    def test_invalid_family(self):
+    def test_invalid_family_length(self):
         form = CheckOutForm(data={"family": "This is a very long name that is over fifty characters"})
 
         self.assertEqual(
             form.errors["family"], ["Ensure this value has at most 50 characters (it has 54)."]
+        )
+
+    def test_invalid_family_exists(self):
+        form = CheckOutForm(data={"family": "InvalidFamily"})
+
+        self.assertEqual(
+            form.errors["family"], ["Family does not exist."]
         )
