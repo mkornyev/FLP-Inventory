92977d0db56880d254781cfeec05c69313664b3c
Lydia Xing <xinglydia@gmail.com>
2021-04-10 13:56:06 -0400

    name exact bug

--- inventory/management/commands/import.py
+++ inventory/management/commands/import.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python
 from django.core.management.base import BaseCommand
 from bs4 import BeautifulSoup
+from inventory.models import Item, Category
 import openpyxl
 
 class Command(BaseCommand):
-    args = 'this function takes three arguments, the paths to the \
-            Sharepoint manage inventory excel file, Sharepoint manage items excel file, \
-            and initial master list of items excel file'
+    args = 'this function takes two arguments, the paths to the \
+            Sharepoint manage inventory excel file, Sharepoint manage items excel file'
     help = 'Download the "Manage Inventory" and "Manage Items"  pages\' from Sharepoint \
-            and the master list of items used to initialize Sharepoint and provide \
-            the path to these files as arguments'
+            and provide the path to these files as arguments'
     
     def add_arguments(self, parser):
         parser.add_argument('manage_inventory')
         parser.add_argument('manage_items')
-        parser.add_argument('masterlist')
 
     def handle(self, *args, **options):
         INVENTORY_FILE_PATH = options['manage_inventory']
         ITEMS_FILE_PATH = options['manage_items']
-        MASTERLIST_PATH = options['masterlist']
 
-        items = dict()
-
-        inventory_sheet = openpyxl.load_workbook(MASTERLIST_PATH).active
+        inventory_sheet = openpyxl.load_workbook(INVENTORY_FILE_PATH).active
         items_sheet = openpyxl.load_workbook(ITEMS_FILE_PATH).active
-        masterlist_sheet = openpyxl.load_workbook(MASTERLIST_PATH).active
+
+        for r in items_sheet.iter_rows(min_row=2):
+            item_name = r[1].value
+            item_category = r[2].value
+            print(f"adding {item_name} w category {item_category}")
+            qs = Category.objects.filter(name__exact="Clothing")
+            if len(qs) == 0:
+                Category.objects.create(name=item_category)
+            else:
+                Item.objects.create(category=qs[0], name=item_name, price=0, quantity=0)
 
         for r in inventory_sheet.iter_rows(min_row=2): # skip header
-            item_name = r[2].value
-            count = r[5].value
-            quantities[item_name] = count
-        for r in items_sheet.iter_rows(min_rows=2):
-            item_name = r[2].value
-            category = r[3].value
-            categories[item_name] = 
-
-        
-            # item1 = Item.objects.create(category=category1, name="tshirt boys 4", price=10.56, quantity=10)
-            # item1.save()
+            item_name = r[1].value
+            quantity = r[4].value
+            print(f"updating quantity {item_name} w {quantity}")
+            qs = Item.objects.filter(name_exact=item_name)
+            if len(qs) != 0:
+                qs[0].quantity = quantity
+                qs[0].save()
+            else:
+                print(f"{item_name} not found")
+            
