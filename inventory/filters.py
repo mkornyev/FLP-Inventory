a0863aba3df190ef4cf3013158cfa2399d7db5f6
Lydia Xing <xinglydia@gmail.com>
2021-04-15 11:41:43 -0400

    fix test

--- inventory/filters.py
+++ inventory/filters.py
@@ -2,6 +2,8 @@ import django_filters
 from .models import Checkout
 
 class CheckoutFilter(django_filters.FilterSet):
+    family__name = django_filters.CharFilter(lookup_expr='iexact')
+
     class Meta:
         model = Checkout
         fields = ['family__name']
