bfd0a38dbd56d165b03ae1d746b15f419ca4eb99
Austin Leung <austinle@andrew.cmu.edu>
2021-04-18 21:55:01 -0400

    Remove register

--- inventory/views.py
+++ inventory/views.py
@@ -65,33 +65,6 @@ def logout_action(request):
     logout(request)
     return redirect(reverse('Login'))
 
-def register_action(request):
-    context = {}
-
-    if request.method == 'GET':
-        context['form'] = RegistrationForm()
-        return render(request, 'inventory/register.html', context)
-
-    form = RegistrationForm(request.POST)
-    context['form'] = form
-
-    if not form.is_valid():
-        return render(request, 'inventory/register.html', context)
-
-    new_user = User.objects.create_user(username=form.cleaned_data['username'], 
-                                        password=form.cleaned_data['password1'],
-                                        email=form.cleaned_data['email'],
-                                        first_name=form.cleaned_data['first_name'],
-                                        last_name=form.cleaned_data['last_name'])
-    new_user.save()
-
-    new_user = authenticate(username=form.cleaned_data['username'],
-                            password=form.cleaned_data['password1'])
-
-    login(request, new_user)
-    return redirect(reverse('Home'))
-
-
 ######################### REPORT GENERATION #########################
 @login_required
 def generate_report(request):
