89cdd4af5a5a2c77a42da07442ac3415d1bd6865
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-04-22 00:09:05 -0400

    Fixed test cases to work for the new checkins and checkouts

--- inventory/test/test_additemview.py
+++ inventory/test/test_additemview.py
@@ -11,53 +11,21 @@ class AddItemTestCase(TestCase):
         user = User.objects.create_superuser(username='testuser', password='12345')
         user.save()
 
-    def test_invalid_not_logged_in(self):
-        response = self.client.get('/additem/in/')
-
-        # Check for redirect
-        self.assertEqual(response.status_code, 302)
-
-        response = self.client.get('/additem/out/')
-
-        # Check for redirect
-        self.assertEqual(response.status_code, 302)
-
-        response = self.client.post('/additem/in/')
-
-        # Check for redirect
-        self.assertEqual(response.status_code, 302)
-
-        response = self.client.post('/additem/out/')
-
-        # Check for redirect
-        self.assertEqual(response.status_code, 302)
-
-    def test_get(self):
-        self.client.login(username='testuser', password='12345')
-
-        response = self.client.get("/additem/in/")
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "<h1>Add Item to Cart:</h1>")
-
-        response = self.client.get("/additem/out/")
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "<h1>Add Item to Cart:</h1>")
-
     def test_post_success(self):
         self.client.login(username='testuser', password='12345')
 
         response = self.client.post(
-            "/additem/in/", data={"name": "ValidItem",
-                                  "quantity": 2}, follow=True
+            "/checkin/", data={"additem": "",
+                               "name": "ValidItem",
+                               "quantity": 2}, follow=True
         )
 
         self.assertEqual(response.status_code, 200)
 
         response = self.client.post(
-            "/additem/out/", data={"name": "ValidItem",
-                                   "quantity": 1}, follow=True
+            "/checkout/", data={"additem": "",
+                                "name": "ValidItem",
+                                "quantity": 1}, follow=True
         )
 
         self.assertEqual(response.status_code, 200)
@@ -73,8 +41,9 @@ class AddItemTestCase(TestCase):
         self.client.login(username='testuser', password='12345')
 
         response = self.client.post(
-            "/additem/in/", data={"name": "InvalidItem",
-                                  "quantity": 1}
+            "/checkin/", data={"additem": "",
+                               "name": "InvalidItem",
+                               "quantity": 1}
         )
 
         self.assertEqual(response.status_code, 200)
