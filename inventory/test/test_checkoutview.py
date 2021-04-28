f74d6ed46dd56a35f4b4c9e58764d5d876646acf
Lydia Xing <xinglydia@gmail.com>
2021-04-28 13:11:35 -0400

    asdf

--- inventory/test/test_checkoutview.py
+++ inventory/test/test_checkoutview.py
@@ -39,7 +39,7 @@ class CheckoutTestCase(TestCase):
     def test_invalid_no_items(self):
         self.client.login(username='testuser', password='12345')
 
-        response = self.client.post('/checkout/', data={"checkout": "", "family":"ValidFamily : (None)", "child": "Big Chungus", "age": "1"}})
+        response = self.client.post('/checkout/', data={"checkout": "", "family":"ValidFamily : (None)", "child": "Big Chungus", "age": "1"})
 
         # Check if invalid
         self.assertEqual(response.status_code, 400)
