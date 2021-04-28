8689f63dc37bd275b7ad8508655abd33caf35c09
SeanEZhou <sezhou@andrew.cmu.edu>
2021-04-27 21:51:25 -0700

    corrected meta merge conflict

--- inventory/models.py
+++ inventory/models.py
@@ -54,14 +54,15 @@ class Category(models.Model):
   
   def __str__(self):
     return "{}".format(self.name)
+  class Meta:
+    verbose_name_plural = "categories"
 
 class AgeRange(models.Model):
   low = models.CharField(max_length=50, blank=False, null=False, unique=True)
   high = models.CharField(max_length=50, blank=False, null=False, unique=True)
   def __str__(self):
     return "{} - {}".format(self.low, self.high)
-  class Meta:
-    verbose_name_plural = "categories"
+  
 
 class Item(models.Model):
   category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True) # CASCADE - deletes all items if a Category is deleted
