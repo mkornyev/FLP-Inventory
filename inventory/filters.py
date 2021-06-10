074ae978b7a79eafc268dfa38961b47ad6494827
Austin Leung <austinle@andrew.cmu.edu>
2021-06-10 00:46:13 -0400

    Item name filter

--- inventory/filters.py
+++ inventory/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Checkout
+from .models import Checkout, Item
 
 class CheckoutFilter(django_filters.FilterSet):
     family__displayName = django_filters.CharFilter(lookup_expr='iexact')
@@ -7,3 +7,10 @@ class CheckoutFilter(django_filters.FilterSet):
     class Meta:
         model = Checkout
         fields = ['family__displayName']
+
+class ItemFilter(django_filters.FilterSet):
+    item__name = django_filters.CharFilter(lookup_expr='iexact')
+
+    class Meta:
+        model = Item
+        fields = ['item__name']
