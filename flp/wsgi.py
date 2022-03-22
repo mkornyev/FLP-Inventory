ec81308300129d8566b3c5a6f37d17006e93a4f2
AashaiAvadhani1 <aashai123@gmail.com>
2022-03-22 10:39:37 -0400

    adding some of the front end and backend tests

--- flp/wsgi.py
+++ flp/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flp.settings.local")
 
 application = get_wsgi_application()
