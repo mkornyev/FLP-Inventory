e9e844283afdb0bfbd5f2159915ae1c1af97cffe
TomasVCMU <tviejobu@andrew.cmu.edu>
2022-03-23 22:05:22 -0400

    Saved credentials feature removed for security purposes

--- inventory/views.py
+++ inventory/views.py
@@ -138,24 +138,10 @@ def generate_report(request):
             return response
 
         if 'export_drive' in request.POST:
-            cwd = os.getcwd()
-            print("TEST: ")
             qs = Checkout.objects.filter(datetime__gte=context['startDate']).filter(datetime__lte=endDatetime).all()
             si = StringIO()
             gauth = GoogleAuth()
-            # Try to load saved client credentials
-            gauth.LoadCredentialsFile("mycreds.txt")
-            if gauth.credentials is None:
-                # Authenticate if they're not there
-                gauth.LocalWebserverAuth()
-            elif gauth.access_token_expired:
-                # Refresh them if expired
-                gauth.Refresh()
-            else:
-                # Initialize the saved creds
-                gauth.Authorize()
-            # Save the current credentials to a file
-            gauth.SaveCredentialsFile("mycreds.txt")
+            gauth.LocalWebserverAuth()
             drive = GoogleDrive(gauth)
             writer = csv.writer(si)
     
