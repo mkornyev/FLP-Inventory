177dee867cc893c1bf6c174dba1c9b224a82dcc9
Tomas Viejobueno <tomasviejobueno@tomass-air.wifi.local.cmu.edu>
2022-04-15 14:44:46 -0400

    Modularizing generate_report

--- inventory/views.py
+++ inventory/views.py
@@ -98,87 +98,17 @@ def generate_report(request):
             context['totalValue'] = result.getValue() + context['totalValue']
 
         if 'export' in request.POST:
-            qs = Checkout.objects.filter(datetime__gte=context['startDate']).filter(datetime__lte=endDatetime).all()
             response = HttpResponse()
             response['Content-Disposition'] = 'attachment; filename=Checkout Report By Item ' + request.POST['start-date'] + " to " + request.POST['end-date'] + '.csv'
-            writer = csv.writer(response)
-    
-            if qs is not None:
-                writer.writerow(["item", "new/used", "category", "quantity", "new/used price", "total value"])
-
-                uniqueItems = {} 
-
-                for c in qs:
-                    for tx in c.items.all():
-                        item_key = (tx.item.id, tx.is_new)
-                        try: 
-                            originalPrice = tx.item.new_price if tx.is_new else tx.item.used_price
-                            adjustedPrice = float(request.POST.get(str(tx.item.id) + '-' + str(tx.is_new) + '-adjustment', originalPrice))
-                        except (ValueError, TypeError) as _: # noqa: F841
-                            adjustedPrice = 0
-
-                        if item_key not in uniqueItems: 
-                            uniqueItems[item_key] = [
-                                tx.item.name,
-                                "New" if tx.is_new else "Used",
-                                "No category" if tx.item.category is None else tx.item.category.name,
-                                tx.quantity,
-                                adjustedPrice,
-                                0 if adjustedPrice is None else round(tx.quantity*adjustedPrice, 2)
-                            ]
-                        else: 
-                            item_key = (tx.item.id, tx.is_new)
-                            uniqueItems[item_key][3] += tx.quantity
-                            uniqueItems[item_key][5] += 0 if adjustedPrice is None else tx.quantity*adjustedPrice
-                            round(uniqueItems[item_key][5], 2)
-                
-                sorted_items = list(sorted(uniqueItems.values(), key=lambda x: (x[0], x[1])))
-                for item in sorted_items:
-                    writer.writerow(item)
-
-            return response
+            return write_export_data(request, context, response)
 
         if 'export_drive' in request.POST:
-            qs = Checkout.objects.filter(datetime__gte=context['startDate']).filter(datetime__lte=endDatetime).all()
             si = StringIO()
             gauth = GoogleAuth()
             gauth.LocalWebserverAuth()
             drive = GoogleDrive(gauth)
-            writer = csv.writer(si)
-    
-            if qs is not None:
-                writer.writerow(["item", "new/used", "category", "quantity", "new/used price", "total value"])
-
-                uniqueItems = {} 
-
-                for c in qs:
-                    for tx in c.items.all():
-                        item_key = (tx.item.id, tx.is_new)
-                        try: 
-                            originalPrice = tx.item.new_price if tx.is_new else tx.item.used_price
-                            adjustedPrice = float(request.POST.get(str(tx.item.id) + '-' + str(tx.is_new) + '-adjustment', originalPrice))
-                        except (ValueError, TypeError) as _: # noqa: F841
-                            adjustedPrice = 0
-
-                        if item_key not in uniqueItems: 
-                            uniqueItems[item_key] = [
-                                tx.item.name,
-                                "New" if tx.is_new else "Used",
-                                "No category" if tx.item.category is None else tx.item.category.name,
-                                tx.quantity,
-                                adjustedPrice,
-                                0 if adjustedPrice is None else round(tx.quantity*adjustedPrice, 2)
-                            ]
-                        else: 
-                            item_key = (tx.item.id, tx.is_new)
-                            uniqueItems[item_key][3] += tx.quantity
-                            uniqueItems[item_key][5] += 0 if adjustedPrice is None else tx.quantity*adjustedPrice
-                            round(uniqueItems[item_key][5], 2)
-                
-                sorted_items = list(sorted(uniqueItems.values(), key=lambda x: (x[0], x[1])))
-                for item in sorted_items:
-                    writer.writerow(item)
-
+            write_export_data(request, context, si)
+            
             fileTitle = context['tx_type'] + 'Report By Item ' + request.POST['start-date'] + " to " + request.POST['end-date'] + '.csv'
             csvFile = drive.CreateFile({'title': fileTitle, 'mimeType': 'text/csv'})
             csvFile.SetContentString(si.getvalue().strip('\r\n'))
@@ -187,134 +117,35 @@ def generate_report(request):
 
         if 'itemizedOutput' in request.POST:
             context['itemizedOutput'] = request.POST['itemizedOutput']
-
-            newUniqueItems = {}
-            for res in context['results']: 
-                for tx in res.items.all(): 
-                    item_key = (tx.item.id, tx.is_new)
-                    item_price = None
-                    if tx.is_new and tx.item.new_price != None:
-                        item_price = tx.item.new_price
-                    elif not tx.is_new and tx.item.used_price != None:
-                        item_price = tx.item.used_price
-                    if item_key not in newUniqueItems:
-                        newUniqueItems[item_key] = {
-                            'id': tx.item.id,
-                            'item': tx.item.name,
-                            'category': "No category" if tx.item.category is None else tx.item.category.name,
-                            'is_new': tx.is_new,
-                            'quantity': tx.quantity,
-                            'new_price': tx.item.new_price,
-                            'used_price': tx.item.used_price,
-                            'value': 0 if item_price is None else tx.quantity*item_price,
-                            'tx_notes': res.notes_description()
-                        }
-                    else: 
-                        newUniqueItems[item_key]['quantity'] += tx.quantity
-                        newUniqueItems[item_key]['value'] += 0 if item_price is None else tx.quantity*item_price
-
-                        currNotes = newUniqueItems[item_key]['tx_notes']
-                        if res.notes and currNotes and str(res.id) not in currNotes: 
-                            currNotes = currNotes + "<br>" + res.notes_description()
-                            newUniqueItems[item_key]['tx_notes'] = currNotes
-
-                        currNotes = newUniqueItems[item_key]['tx_notes']
-                        if res.notes and currNotes and str(res.id) not in currNotes: 
-                            currNotes = currNotes + "<br>" + res.notes_description()
-                            newUniqueItems[item_key]['tx_notes'] = currNotes
-
-            context['results'] = list(sorted(newUniqueItems.values(), key=lambda x: (x['item'], "New" if x['is_new'] else "Used")))
-
+            collect_itemized_data(context)
+           
         if 'export_table' not in request.POST \
             and 'export_drive_table' not in request.POST:
             context['results'] = getPagination(request, context['results'], DEFAULT_PAGINATION_SIZE)
             return render(request, 'inventory/reports/generate_report.html', context)
 
         if 'export_table' in request.POST:
-            qs = context['results']
             response = HttpResponse()
-            response['Content-Disposition'] = 'attachment; filename=' + context['tx_type'] + ' Report ' + request.POST['start-date'] + " to " + request.POST['end-date'] + '.csv'
-            writer = csv.writer(response)
 
             if 'itemizedOutput' in request.POST:
                 response['Content-Disposition'] = 'attachment; filename=' + context['tx_type'] + ' Report By Item ' + request.POST['start-date'] + " to " + request.POST['end-date'] + '.csv'
-                if len(context.get('results', [])) != 0:
-                    headers = list(context['results'][0].keys())
-                    headers = [x for x in headers if x not in ['tx_notes', 'new_price', 'used_price']]
-                    headers.append('new/used price')
-                    writer.writerow(headers)
-                    for i in context['results']:
-                        row = []
-                        for h in headers:
-                            if h == "new/used price":
-                                if i['is_new']:
-                                    row.append(i['new_price'])
-                                else:
-                                    row.append(i['used_price'])
-                            else:
-                                row.append(i[h])
-                        writer.writerow(row)
-                return response
+            else:
+                response['Content-Disposition'] = 'attachment; filename=' + context['tx_type'] + ' Report ' + request.POST['start-date'] + " to " + request.POST['end-date'] + '.csv'
 
-            if len(qs) != 0:
-                field_names = [f.name for f in qs.model._meta.get_fields()] + ["value"]
-                writer.writerow(field_names)
-                for i in qs:
-                    row = []
-                    for f in field_names:
-                        if f == "items":
-                            txs = ', '.join([str(tx) for tx in i.items.all()])
-                            row.append(txs)
-                        elif f == "value":
-                            row.append(i.getValue())
-                        else:
-                            row.append(getattr(i, f))
-                    writer.writerow(row)
-            return response
+            return write_export_table_data(request, context, response)
 
         if 'export_drive_table' in request.POST:
             si = StringIO()
             gauth = GoogleAuth()
             gauth.LocalWebserverAuth()
             drive = GoogleDrive(gauth)
-            qs = context['results']
-            writer = csv.writer(si)
-            fileTitle = ""
+            write_export_table_data(request, context, si)
 
-            if 'itemizedOutput' in request.POST:  
+            if 'itemizedOutput' in request.POST:
                 fileTitle = context['tx_type'] + 'Report By Item ' + request.POST['start-date'] + " to " + request.POST['end-date'] + '.csv'
-                if len(context.get('results', [])) != 0:
-                    headers = list(context['results'][0].keys())
-                    headers = [x for x in headers if x not in ['tx_notes', 'new_price', 'used_price']]
-                    headers.append('new/used price')
-                    writer.writerow(headers)
-                    for i in context['results']:
-                        row = []
-                        for h in headers:
-                            if h == "new/used price":
-                                if i['is_new']:
-                                    row.append(i['new_price'])
-                                else:
-                                    row.append(i['used_price'])
-                            else:
-                                row.append(i[h])
-                        writer.writerow(row)
-
-            if len(qs) != 0 and 'itemizedOutput' not in request.POST:
+            else:
                 fileTitle = context['tx_type'] + 'Report ' + request.POST['start-date'] + " to " + request.POST['end-date'] + '.csv'
-                field_names = [f.name for f in qs.model._meta.get_fields()] + ["value"]
-                writer.writerow(field_names)
-                for i in qs:
-                    row = []
-                    for f in field_names:
-                        if f == "items":
-                            txs = ', '.join([str(tx) for tx in i.items.all()])
-                            row.append(txs)
-                        elif f == "value":
-                            row.append(i.getValue())
-                        else:
-                            row.append(getattr(i, f))
-                    writer.writerow(row)
+
             csvFile = drive.CreateFile({'title': fileTitle, 'mimeType': 'text/csv'})
             csvFile.SetContentString(si.getvalue().strip('\r\n'))
             csvFile.Upload()
@@ -326,16 +157,125 @@ def generate_report(request):
     context['startDate'] = weekAgo.strftime('%Y-%m-%d')
     return render(request, 'inventory/reports/generate_report.html', context)
 
-# def collect_itemized_data():
+def collect_itemized_data(context):
+    newUniqueItems = {}
+    for res in context['results']: 
+        for tx in res.items.all(): 
+            item_key = (tx.item.id, tx.is_new)
+            item_price = None
+            if tx.is_new and tx.item.new_price != None:
+                item_price = tx.item.new_price
+            elif not tx.is_new and tx.item.used_price != None:
+                item_price = tx.item.used_price
+            if item_key not in newUniqueItems:
+                newUniqueItems[item_key] = {
+                    'id': tx.item.id,
+                    'item': tx.item.name,
+                    'category': "No category" if tx.item.category is None else tx.item.category.name,
+                    'is_new': tx.is_new,
+                    'quantity': tx.quantity,
+                    'new_price': tx.item.new_price,
+                    'used_price': tx.item.used_price,
+                    'value': 0 if item_price is None else tx.quantity*item_price,
+                    'tx_notes': res.notes_description()
+                }
+            else: 
+                newUniqueItems[item_key]['quantity'] += tx.quantity
+                newUniqueItems[item_key]['value'] += 0 if item_price is None else tx.quantity*item_price
 
-# def write_itemized_data():
+                currNotes = newUniqueItems[item_key]['tx_notes']
+                if res.notes and currNotes and str(res.id) not in currNotes: 
+                    currNotes = currNotes + "<br>" + res.notes_description()
+                    newUniqueItems[item_key]['tx_notes'] = currNotes
 
-# def write_export_table_data():
+                currNotes = newUniqueItems[item_key]['tx_notes']
+                if res.notes and currNotes and str(res.id) not in currNotes: 
+                    currNotes = currNotes + "<br>" + res.notes_description()
+                    newUniqueItems[item_key]['tx_notes'] = currNotes
+                
+    context['results'] = list(sorted(newUniqueItems.values(), key=lambda x: (x['item'], "New" if x['is_new'] else "Used")))
 
-# def write_export_data():
+def write_export_table_data(request, context, csvObj):
+    qs = context['results']
+    writer = csv.writer(csvObj)
+
+    if 'itemizedOutput' in request.POST:
+        csvObj['Content-Disposition'] = 'attachment; filename=' + context['tx_type'] + ' Report By Item ' + request.POST['start-date'] + " to " + request.POST['end-date'] + '.csv'
+        if len(context.get('results', [])) != 0:
+            headers = list(context['results'][0].keys())
+            headers = [x for x in headers if x not in ['tx_notes', 'new_price', 'used_price']]
+            headers.append('new/used price')
+            writer.writerow(headers)
+            for i in context['results']:
+                row = []
+                for h in headers:
+                    if h == "new/used price":
+                        if i['is_new']:
+                            row.append(i['new_price'])
+                        else:
+                            row.append(i['used_price'])
+                    else:
+                        row.append(i[h])
+                writer.writerow(row)
+        return csvObj
+
+    if len(qs) != 0:
+        field_names = [f.name for f in qs.model._meta.get_fields()] + ["value"]
+        writer.writerow(field_names)
+        for i in qs:
+            row = []
+            for f in field_names:
+                if f == "items":
+                    txs = ', '.join([str(tx) for tx in i.items.all()])
+                    row.append(txs)
+                elif f == "value":
+                    row.append(i.getValue())
+                else:
+                    row.append(getattr(i, f))
+            writer.writerow(row)
+    return csvObj
+
+def write_export_data(request, context, csvObj):
+    endDatetime = datetime.strptime('{} 23:59:59'.format(context['endDate']), '%Y-%m-%d %H:%M:%S')
+    qs = Checkout.objects.filter(datetime__gte=context['startDate']).filter(datetime__lte=endDatetime).all()
+    writer = csv.writer(csvObj)
+
+    if qs is not None:
+        writer.writerow(["item", "new/used", "category", "quantity", "new/used price", "total value"])
+
+        uniqueItems = {} 
+
+        for c in qs:
+            for tx in c.items.all():
+                item_key = (tx.item.id, tx.is_new)
+                try: 
+                    originalPrice = tx.item.new_price if tx.is_new else tx.item.used_price
+                    adjustedPrice = float(request.POST.get(str(tx.item.id) + '-' + str(tx.is_new) + '-adjustment', originalPrice))
+                except (ValueError, TypeError) as _: # noqa: F841
+                    adjustedPrice = 0
+
+                if item_key not in uniqueItems: 
+                    uniqueItems[item_key] = [
+                        tx.item.name,
+                        "New" if tx.is_new else "Used",
+                        "No category" if tx.item.category is None else tx.item.category.name,
+                        tx.quantity,
+                        adjustedPrice,
+                        0 if adjustedPrice is None else round(tx.quantity*adjustedPrice, 2)
+                    ]
+                else: 
+                    item_key = (tx.item.id, tx.is_new)
+                    uniqueItems[item_key][3] += tx.quantity
+                    uniqueItems[item_key][5] += 0 if adjustedPrice is None else tx.quantity*adjustedPrice
+                    round(uniqueItems[item_key][5], 2)
+        
+        sorted_items = list(sorted(uniqueItems.values(), key=lambda x: (x[0], x[1])))
+        for item in sorted_items:
+            writer.writerow(item)
+
+    return csvObj
 
 # def google_auth():
-
 
 ######################### ANALYTICS #########################
 @login_required
